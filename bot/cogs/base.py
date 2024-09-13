83e28821bb1a4c965d0c6e7699aa734e44a0ea01
brahm <brahmwongwandanee@gmail.com>
2024-09-12 21:47:53 -0700

    updated 2 commands

--- bot/cogs/base.py
+++ bot/cogs/base.py
@@ -10,19 +10,13 @@ class Base(commands.Cog):
     async def help(self, ctx):
         embed = discord.Embed(
             title='(น้องบราห์ม) nong brahm',
-            description='hey im nong brahm!\n[Invite Nong Brahm!](https://discord.com/oauth2/authorize?client_id=992708824727293952&permissions=8&scope=bot)',
+            description='hey im nong brahm!\n[invite me!](https://discord.com/oauth2/authorize?client_id=992708824727293952&permissions=8&scope=bot)\n[my discord!](https://discord.gg/QN4KfD4zsc)',
             color=discord.Colour.from_rgb(255, 74, 119)
         ) \
             .set_thumbnail(url='https://i.imgur.com/bmmFpaf.jpg') \
-            .add_field(name="Links", value="""
-[Wiki](https://github.com/MrBrahm/nongbrahm/wiki)
-[Commands](https://github.com/MrBrahm/nongbrahm/wiki/Commands)
-[Github](https://github.com/MrBrahm/nongbrahm)
-[Support Server](https://discord.gg/QN4KfD4zsc)
-            """) \
-            .add_field(name="Servers", value=f"{len(self.bot.guilds)}") \
-            .add_field(name="Credits", value="programming by brahm") \
-            .set_footer(text=f'ID: {self.bot.user.id}')
+            .add_field(name="guilds", value=f"{len(self.bot.guilds)}") \
+            .add_field(name="bot credits", value="brahm") \
+            .set_footer(text=f'bot user id: {self.bot.user.id}')
         await ctx.send(embed=embed)
 
 
